fed91191f5d6baba18661d6bc298cd088bd882f0
Thet Htar Thu Zar <thethtarthuzar648@gmail.com>
2024-05-07 21:55:00 +0630

    Add project files.

---
+++ ThetHtarThuZarDotNetCore.RestApi/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThetHtarThuZarDotNetCore.RestApi.Controllers
+{
+    //api/User
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Greeting()
+        {
+            //return Ok("Welcome");
+
+            return StatusCode(StatusCodes.Status200OK, "Welcome!");
+        }
+    }
+}
